13e56567a6b66c3997fbb804e86ca5754ba99c19
trthhrtz <k.leshakov@yandex.ru>
2017-07-12 12:12:08 +0000

    First try of sever_launch

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
-sudo ﻿ln -sf /home/box/web/stepic_web_project/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/stepic_web_project/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+﻿
